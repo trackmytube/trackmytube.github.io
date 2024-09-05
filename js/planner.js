2b4ccfe76b7f96dc10344bc01faa033d73344749
jahir10ali <jahir10ali@gmail.com>
2024-09-05 09:12:48 +0100

    Renamed the planner files

--- js/planner.js
+++ js/planner.js
@@ -8,21 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const switchButton = document.getElementById('switch-stations');
     const searchButton = document.getElementById('search-button');
     const journeyPreferencesButton = document.getElementById('journey-preferences-button');
-    const journeyPreferences = document.getElementById('journey-preferences');
+    const journeyPreferences = document.querySelector('.journey-preferences-section');
     const datePicker = document.getElementById('date-picker');
     const timePicker = document.getElementById('time-picker');
     const timeIsSelect = document.getElementById('time-is');
     const journeyPreferenceSelect = document.getElementById('journey-preference');
     const journeyResults = document.getElementById('journey-results');
+    const clearJourneyPreferencesButton = document.getElementById('clear-journey-preferences');
 
     let allStations = [];
     let activeInput = null;
     let stationFrom = "";
     let stationTo = "";
     let stationVia = "";
-    let naptanFrom = ""; 
-    let naptanTo = ""; 
-    let naptanVia = "";
+    let naptanFrom = [];
+    let naptanTo = [];
+    let naptanVia = [];
 
     let date = "";
     let time = "";
@@ -63,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function fetchStations() {
         try {
-            const response = await fetch('https://raw.githubusercontent.com/jahir10ali/naptans-for-tfl-stations/main/data/Station_NaPTANs.json');
+            const response = await fetch('https://raw.githubusercontent.com/jahir10ali/naptans-for-tfl-stations/main/data/tfl-station-naptan-data.json');
             if (!response.ok) throw new Error('Network response was not ok');
             allStations = await response.json();
         } catch (error) {
@@ -85,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const colors = station.Lines.map(line => lineColors[line] || '#000000');
                 const colorRectangles = colors.map(color => `<span class="line-color" style="background-color: ${color};"></span>`).join(' ');
                 return `
-                    <div class="station-result" data-station-name="${station['Station Name']}" data-station-lines='${JSON.stringify(station.Lines)}' data-naptan-id="${station['NaPTAN ID']}">
+                    <div class="station-result" data-station-name="${station['Station Name']}" data-station-lines='${JSON.stringify(station.Lines)}' data-naptan-id='${JSON.stringify(station['NaPTAN ID'])}'>
                         <h3>${station['Station Name']}</h3>
                         <div class="line-colors">${colorRectangles}</div>
                     </div>
@@ -100,20 +101,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const result = event.target.closest('.station-result');
         if (result) {
             const stationName = result.dataset.stationName;
-            const naptanId = result.dataset.naptanId;
+            const naptanIds = JSON.parse(result.dataset.naptanId);
             searchResults.style.display = 'none';
 
             if (activeInput === searchInputOne) {
                 stationFrom = stationName;
-                naptanFrom = naptanId;
+                naptanFrom = naptanIds;
                 searchInputOne.value = stationName;
             } else if (activeInput === searchInputTwo) {
                 stationTo = stationName;
-                naptanTo = naptanId;
+                naptanTo = naptanIds;
                 searchInputTwo.value = stationName;
             } else if (activeInput === searchInputVia) {
                 stationVia = stationName;
-                naptanVia = naptanId;
+                naptanVia = naptanIds;
                 searchInputVia.value = stationName;
             }
 
@@ -123,7 +124,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function updateJourneyTitle() {
         if (stationFrom && stationTo) {
-            selectedStation.innerHTML = `<h5>${stationFrom} &rarr; ${stationTo}</h5>`;
+            selectedStation.innerHTML = `<h3>${stationFrom} &rarr; ${stationTo}</h3>`;
             selectedStation.style.display = 'block'; 
         }
     }
@@ -135,10 +136,11 @@ document.addEventListener('DOMContentLoaded', function () {
         searchInputTwo.value = stationTo;
 
         clearJourneyResults(); 
+        hideJourneyTitleAndResults();
     }
 
     function toggleJourneyPreferences() {
-        journeyPreferences.classList.toggle('hidden');
+        journeyPreferences.classList.toggle('visible');
     }
 
     function collectJourneyPreferences() {
@@ -154,53 +156,103 @@ document.addEventListener('DOMContentLoaded', function () {
         journeyResults.innerHTML = '';
     }
 
+    function hideJourneyTitleAndResults() {
+        selectedStation.style.display = 'none';
+        journeyResults.innerHTML = '';
+    }
 
-    function buildJourneyUrl() {
-        let url = `https://api.tfl.gov.uk/Journey/JourneyResults/${naptanFrom}/to/${naptanTo}?&mode=tube,overground,elizabeth-line,tram,dlr,national-rail&useRealTimeLiveArrivals=true&nationalSearch=true&walkingSpeed=Average`;
+    function buildJourneyUrls() {
+        let urls = [];
 
-        if (naptanVia) {
-            url += `&via=${naptanVia}`;
-        }
+        naptanFrom.forEach(fromId => {
+            naptanTo.forEach(toId => {
+                let url = `https://api.tfl.gov.uk/Journey/JourneyResults/${fromId}/to/${toId}?&mode=tube,overground,elizabeth-line,tram,dlr,national-rail&useRealTimeLiveArrivals=true&nationalSearch=true&walkingSpeed=Average`;
+
+                if (naptanVia.length > 0) {
+                    naptanVia.forEach(viaId => {
+                        urls.push(`${url}&via=${viaId}`);
+                    });
+                } else {
+                    urls.push(url);
+                }
+            });
+        });
 
         if (date) {
             const formattedDate = date.replace(/-/g, ''); 
-            url += `&date=${formattedDate}`;
+            urls = urls.map(url => `${url}&date=${formattedDate}`);
         }
         if (time) {
             const formattedTime = time.replace(/:/g, ''); 
-            url += `&time=${formattedTime}`;
+            urls = urls.map(url => `${url}&time=${formattedTime}`);
         }
 
         if (timeIs) {
-            url += `&timeIs=${timeIs}`;
+            urls = urls.map(url => `${url}&timeIs=${timeIs}`);
         }
 
         if (journeyPreference) {
-            url += `&journeyPreference=${journeyPreference}`;
+            urls = urls.map(url => `${url}&journeyPreference=${journeyPreference}`);
         }
-        return url;
+
+        return urls;
     }
 
-
     async function fetchJourneyResults() {
-        if (!naptanFrom || !naptanTo) {
+        if (!naptanFrom.length || !naptanTo.length) {
             alert('Please enter both FROM and TO Stations.');
             return;
         }
-
-        journeyResults.innerHTML = '<p>Fetching journey details...</p>'; 
-
+    
+        journeyResults.innerHTML = '<p>Fetching journey details...</p>';
+    
+        const urls = buildJourneyUrls();
+        const fetchPromises = urls.map(url => fetch(url).then(response => {
+            if (!response.ok) {
+                return response.json().then(data => {
+                    if (data.httpStatusCode === 404 && data.exceptionType === "EntityNotFoundException") {
+                        throw new Error(data.message || 'No journey found for your inputs.');
+                    } else {
+                        throw new Error('Error fetching journey details. Please try again.');
+                    }
+                });
+            }
+            return response.json();
+        }));
+    
         try {
-            const url = buildJourneyUrl();
-            const response = await fetch(url);
-            if (!response.ok) throw new Error('Network response was not ok');
-            const data = await response.json();
-
-            console.log('API Response Data:', data);
-            displayJourneyResults(data);
+            const results = await Promise.all(fetchPromises);
+            let journeys = results.flatMap(result => result.journeys || []);
+    
+            // Filter out journeys where the first leg has an empty name in routeOptions
+            journeys = journeys.filter(journey => {
+                const firstLeg = journey.legs[0]; // Get the first leg of the journey
+                const routeOptions = firstLeg.routeOptions || []; // Get routeOptions of the first leg
+                return routeOptions.length > 0 && routeOptions[0].name.trim() !== ""; // Check if name is not empty
+            });
+    
+            // Remove duplicate journeys based on startDateTime and arrivalDateTime
+            const uniqueJourneys = [];
+            const seenJourneys = new Set();
+    
+            journeys.forEach(journey => {
+                const key = `${journey.startDateTime}|${journey.arrivalDateTime}`;
+                if (!seenJourneys.has(key)) {
+                    uniqueJourneys.push(journey);
+                    seenJourneys.add(key);
+                }
+            });
+    
+            console.log(uniqueJourneys); // Log the filtered and deduplicated journeys
+    
+            if (uniqueJourneys.length === 0) {
+                journeyResults.innerHTML = 'We are experiencing issues with the journey data. Please try again later.';
+            } else {
+                displayJourneyResults(uniqueJourneys);
+            }
         } catch (error) {
-            console.error('Error fetching journey results:', error);
-            journeyResults.innerHTML = 'Error fetching journey details. Please try again.';
+            console.error('Error fetching journey details:', error);
+            journeyResults.innerHTML = error.message;
         }
     }
 
@@ -208,47 +260,70 @@ document.addEventListener('DOMContentLoaded', function () {
         return new Date(time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     }
 
-    function displayJourneyResults(data) {
-        if (data && data.journeys && Array.isArray(data.journeys) && data.journeys.length > 0) {
-            journeyResults.innerHTML = data.journeys.map(journey => {
+    function formatDuration(minutes) {
+        const hours = Math.floor(minutes / 60);
+        const mins = minutes % 60;
+        if (hours > 0) {
+            return `${hours} hr${hours > 1 ? 's' : ''} ${mins} min`;
+        } else {
+            return `${mins} min`;
+        }
+    }
+
+    function displayJourneyResults(journeys) {
+        if (journeys.length > 0) {
+            journeyResults.innerHTML = journeys.map(journey => {
                 const legsHtml = journey.legs && Array.isArray(journey.legs) ? journey.legs.map(leg => {
                     const trainName = leg.routeOptions?.[0]?.name || 'Interchange';
                     const departureStopName = leg.departurePoint?.commonName || 'Unknown Departure Stop';
                     const arrivalStopName = leg.arrivalPoint?.commonName || 'Unknown Arrival Stop';
                     const departureTime = leg.departureTime ? formatTime(leg.departureTime) : 'Unknown Time';
                     const arrivalTime = leg.arrivalTime ? formatTime(leg.arrivalTime) : 'Unknown Time';
-                    const duration = leg.duration || 'Unknown Duration';
+                    const duration = leg.duration ? formatDuration(leg.duration) : 'Unknown Duration';
                     const stopPoints = leg.path?.stopPoints || [];
-                    const stopPointsHtml = stopPoints?.map(stop => `
-                            <li>${stop.name}</li>
-                        `).join('') || '';
-                    
-
-                    const lineColor = lineColors[trainName] || false; 
-                    
-                    return `
-                        <h3><strong>${trainName}</strong> <span class="line-color-inline" style="background-color: ${lineColor};"></span></h3>
-                        <div class="journey-leg" style="border-color: ${lineColor};"> <!-- Set border color dynamically -->
-                            <div class="journey-leg-content">
-                                <p class="journey-leg-station"><strong>${departureTime} - ${departureStopName}</strong></p>
-                                <p><strong>${duration} min</strong> <button class="view-stops-button">View Stops (${stopPoints.length})</button></p>
-                                <ul class="stop-points" style="display: none;">
-                                    ${stopPointsHtml}
-                                </ul>
-                                <p class="journey-leg-station"><strong>${arrivalTime} - ${arrivalStopName}</strong></p>
+                    const stopPointsHtml = stopPoints.map(stop => `<li>${stop.name}</li>`).join('');
+    
+                    const lineColor = lineColors[trainName] || '#000'; 
+    
+                    if (trainName != "Interchange") {
+                        return `
+                            <h3><strong>${trainName}</strong> <span class="line-color-inline" style="background-color: ${lineColor};"></span></h3>
+                            <div class="journey-leg" style="border-color: ${lineColor};">
+                                <div class="journey-leg-content">
+                                    <p class="journey-leg-station"><strong>${departureTime} - ${departureStopName}</strong></p>
+                                    <p><strong>${duration}</strong> <button class="view-stops-button">View Stops (${stopPoints.length})</button></p>
+                                    <ul class="stop-points" style="display: none;">
+                                        ${stopPointsHtml}
+                                    </ul>
+                                    <p class="journey-leg-station"><strong>${arrivalTime} - ${arrivalStopName}</strong></p>
+                                </div>
                             </div>
-                        </div>
-                    `;
+                        `;
+                    } else {
+                        return `
+                            <h3><strong>${trainName}</strong></h3>
+                            <div class="journey-leg" style="border-color: ${lineColor};">
+                                <div class="journey-leg-content">
+                                    <p class="journey-leg-station"><strong>${departureStopName}</strong></p>
+                                    <p class="journey-leg-station"><strong>${arrivalStopName}</strong></p>
+                                </div>
+                            </div>
+                        `;
+                    }
                 }).join('') : 'No legs data available';
 
+                const journeyDuration = journey.duration ? formatDuration(journey.duration) : 'Unknown Duration';
+                const journeyChanges = journey.legs.length - 1 === 0 ? 'Direct' : `${journey.legs.length - 1} change(s)`;
+    
                 return `
                     <div class="journey-item">
-                        <h3>Duration: ${journey.duration ? `${journey.duration} min` : 'Unknown Duration'}, ${formatTime(journey.startDateTime)} - ${formatTime(journey.arrivalDateTime)}, ${journey.legs.length-1} change(s)</h3>
+                        <h3>Duration: ${journeyDuration}, ${formatTime(journey.startDateTime)} - ${formatTime(journey.arrivalDateTime)}, ${journeyChanges}</h3>
                         ${legsHtml}
                     </div>
                 `;
             }).join('');
-
+    
+            // Add click event listeners to view stops buttons
             document.querySelectorAll('.view-stops-button').forEach(button => {
                 button.addEventListener('click', () => {
                     const stopPointsList = button.closest('.journey-leg-content').querySelector('.stop-points');
@@ -279,6 +354,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
             clearJourneyResults();
         });
+
+        // Add clear button functionality
+        const clearButton = input.nextElementSibling; // Assuming the clear button is immediately after the input in HTML structure
+        clearButton.addEventListener('click', function () {
+            input.value = ''; // Clear the input
+            input.dispatchEvent(new Event('input')); // Trigger the input event to clear results
+
+            if (input === searchInputOne) {
+                stationFrom = "";
+                naptanFrom = [];
+            } else if (input === searchInputTwo) {
+                stationTo = "";
+                naptanTo = [];
+            } else if (input === searchInputVia) {
+                stationVia = "";
+                naptanVia = [];
+            }
+    
+            clearJourneyResults();
+        });
+    });
+
+    clearJourneyPreferencesButton.addEventListener('click', function() {
+        searchInputVia.value = '';
+        datePicker.value = '';
+        timePicker.value = '';
+        timeIsSelect.value = 'departing';
+        journeyPreferenceSelect.value = 'leastinterchange';
+
+        stationVia = "";
+        naptanVia = [];
+        date = '';
+        time = '';
+        timeIs = 'departing';
+        journeyPreference = 'leastinterchange';
+
+        clearJourneyResults();
+        hideJourneyTitleAndResults();
     });
 
     searchButton.addEventListener('click', function() {
@@ -286,12 +399,35 @@ document.addEventListener('DOMContentLoaded', function () {
         fetchJourneyResults();
     });
 
-    switchButton.addEventListener('click', switchStations);
-    journeyPreferencesButton.addEventListener('click', toggleJourneyPreferences);
-    datePicker.addEventListener('change', collectJourneyPreferences);
-    timePicker.addEventListener('change', collectJourneyPreferences);
-    timeIsSelect.addEventListener('change', collectJourneyPreferences);
-    journeyPreferenceSelect.addEventListener('change', collectJourneyPreferences);
+    journeyPreferencesButton.addEventListener('click', function() {
+        toggleJourneyPreferences();
+    });
+
+    switchButton.addEventListener('click', function() {
+        switchStations();
+        hideJourneyTitleAndResults(); 
+    });
+
+    datePicker.addEventListener('change', function() {
+        collectJourneyPreferences();
+        hideJourneyTitleAndResults(); 
+    });
+
+    timePicker.addEventListener('change', function() {
+        collectJourneyPreferences();
+        hideJourneyTitleAndResults(); 
+    });
+
+    timeIsSelect.addEventListener('change', function() {
+        collectJourneyPreferences();
+        hideJourneyTitleAndResults(); 
+    });
+
+    journeyPreferenceSelect.addEventListener('change', function() {
+        collectJourneyPreferences();
+        hideJourneyTitleAndResults(); 
+    });
+
     searchResults.addEventListener('click', handleStationClick);
 
     fetchStations();
